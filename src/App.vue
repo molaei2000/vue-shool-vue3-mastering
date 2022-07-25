275d216a26b8afe185e88eb1b19507b6540402fd
mohammad mahdi <mahdi.molaei2000@gmail.com>
2022-07-25 18:31:36 +0430

    finish chapter 9 by adding spinner

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,50 @@
 <template>
   <the-navbar/>
   <div class="container">
-    <router-view/>
+    <router-view v-show="showPage" @ready="onPageReady"/>
+    <AppSpinner v-show="!showPage">loading...</AppSpinner>
   </div>
 </template>
 
 <script>
 import TheNavbar from '@/components/TheNavbar'
 import { mapActions } from 'vuex'
+import NProgress from 'nprogress'
+
 export default {
   name: 'App',
   components: { TheNavbar },
   methods: {
-    ...mapActions(['fetchAuthUser'])
+    ...mapActions(['fetchAuthUser']),
+    onPageReady () {
+      this.showPage = true
+      NProgress.done()
+    }
+  },
+  data () {
+    return {
+      showPage: false
+    }
   },
   created () {
+    NProgress.configure({
+      speed: 200,
+      showSpinner: false
+    })
     this.fetchAuthUser()
+    this.$router.beforeEach(() => {
+      this.showPage = false
+      NProgress.start()
+    })
   }
 }
 </script>
 
 <style>
 @import "assets/style.css";
+@import "~nprogress/nprogress.css";
+
+#nprogress .bar{
+  background: #57AD8D !important;
+}
 </style>
